e36a8ccd39c9e01b0f6045964219cd49465560b0
Felipe Rocha <felipergomes19@gmail.com>
2025-03-27 08:27:21 -0300

    Adiciona a pasta 'Aula-03' além de corrigir a organização das pastas

---
+++ Aula-03/seila.js
@@ -0,0 +1,11 @@
+// function Somar (a, b) 
+// {
+//     return a + b;
+// }
+//  console.log(Somar(5,"10"));
+function SomarComTipo(a, b) {
+    return a + b;
+}
+console.log(SomarComTipo(5, 10));
+//  Somar("oi","tudo bem");
+//  SomarComTipo("oi", "tudo bem ")
---
+++ Aula-03/seila.ts
@@ -0,0 +1,12 @@
+// function Somar (a, b) 
+// {
+//     return a + b;
+// }
+//  console.log(Somar(5,"10"));
+
+ function SomarComTipo(a: number, b: number): number {
+    return a + b;
+ }
+ console.log(SomarComTipo(5, 10));
+//  Somar("oi","tudo bem");
+//  SomarComTipo("oi", "tudo bem ")
